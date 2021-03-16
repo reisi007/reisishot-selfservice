0819c7df98675ab4987c03148a494cf58951ed35
Florian Reisinger <reisi007@gmail.com>
2021-03-16 17:15:24 +0100

    Send mails as UTF-8

--- utils/mail.php
+++ utils/mail.php
@@ -2,14 +2,8 @@
 
 function sendMail(string $from, string $to, string $subject, string $body): bool
 {
-    $from = utf8_decode($from);
-    $to = utf8_decode($to);
-    $subject = utf8_decode($subject);
-    $body = utf8_decode($body);
-
-
     $headers = 'MIME-Version: 1.0' . "\r\n";
-    $headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+    $headers .= 'Content-type: text/html; charset=utf-8' . "\r\n";
     // Create email headers
     $headers .= 'From: ' . $from . "\r\n" .
         'Reply-To: ' . $from . "\r\n" .
