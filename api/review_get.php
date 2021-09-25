e8dac87a803c224c951fa7b15553b130f0b69f93
Florian Reisinger <reisi007@gmail.com>
2021-09-25 10:04:54 +0200

    Store reviews + minor refactoring

--- api/review_get.php
+++ api/review_get.php
@@ -1,5 +1,5 @@
 <?php
-include_once "../db/fetch_multiple_secured.php";
+include_once "../db/fetch_single_secured.php";
 
 query("
 SELECT * FROM review WHERE email = :email AND access_key = :access_key
