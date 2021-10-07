a9f46db85362074d2df0f920f582fe4ad97a9d67
Florian Reisinger <reisi007@gmail.com>
2021-10-07 22:08:43 +0200

    Fetch all reviews

---
+++ api/reviews-admin_get.php
@@ -0,0 +1,21 @@
+<?php
+include_once "../header/json.php";
+include_once "../utils/sql.php";
+include_once "../db/fetch_multiple.php";
+include_once "../utils/security.php";
+
+
+$headers = getallheaders();
+
+$user = trim($headers["Email"]);
+$pwd = trim($headers["Accesskey"]);
+
+$pdo = createMysqlConnection();
+
+// Check if user is allowed to insert
+if (!checkUserInsert($pdo, $user, $pwd)) {
+    throw new Exception("Wrong PWD");
+}
+
+query("SELECT * FROM reviews");
+
