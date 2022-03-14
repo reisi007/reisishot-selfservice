acd6912091e45111c5e1a7a3e3fa4463857e64bb
Florian Reisinger <reisi007@gmail.com>
2022-03-14 15:22:03 +0100

    break didn't work as expected

--- utils/calendar.php
+++ utils/calendar.php
@@ -14,8 +14,7 @@ include_once '../config/calender.conf.php';
 function accessCalendar($resultEntryCreator, $mergeEntries): void
 {
     $iCal = new iCal(calendar_shooting);
-    $curKwDt = new DateTime('today');
-    $curKw = intval($curKwDt->format("W"));
+    $curDt = new DateTime('yesterday');
 
     $events = $iCal->events();
 
@@ -25,25 +24,24 @@ function accessCalendar($resultEntryCreator, $mergeEntries): void
         $dateStart = $event->dateStart;
         $dateEnd = $event->dateEnd;
         $heading = $event->summary;
+
         $isShooting = !str_contains(strtolower($heading), "pause");
 
         $startsDt = new DateTime($dateStart);
         $endDt = new DateTime($dateEnd);
 
-        $endDt->modify("-1 second");
-        $startKw = intval($startsDt->format("W"));
-        $endKw = intval($endDt->format("W"));
+        if ($endDt >= $curDt) {
+            $endDt->modify("-1 second");
+            $startKw = intval($startsDt->format("W"));
+            $endKw = intval($endDt->format("W"));
 
-        if ($curKw > $endKw) {
-            break;
-        }
-
-        for ($kw = $startKw; $kw <= $endKw; $kw++) {
-            $cur = $resultEntryCreator($kw, $isShooting, $event);
-            if (array_key_exists($kw, $result)) {
-                $cur = $mergeEntries($result[$kw], $cur);
+            for ($kw = $startKw; $kw <= $endKw; $kw++) {
+                $cur = $resultEntryCreator($kw, $isShooting, $event);
+                if (array_key_exists($kw, $result)) {
+                    $cur = $mergeEntries($result[$kw], $cur);
+                }
+                $result[$kw] = $cur;
             }
-            $result[$kw] = $cur;
         }
     }
 
