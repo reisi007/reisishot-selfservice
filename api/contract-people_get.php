7254e2e796b867ff81db697cae84f04ff8e2344f
Florian Reisinger <reisi007@gmail.com>
2022-04-22 19:40:08 +0200

    Fix contract admin APIs

--- api/contract-people_get.php
+++ api/contract-people_get.php
@@ -1,16 +1,5 @@
 <?php
-include_once "../db/fetch_multiple.php";
-include_once "../utils/security.php";
-
-$connection = createMysqlConnection();
-
-$headers = getallheaders();
-$user = strtolower(trim($headers["Email"]));
-$pwd = trim($headers["Accesskey"]);
-
-if (!checkUserInsert($connection, $user, $pwd)) {
-    throw new Exception("Wrong PWD");
-}
+include_once "../db/fetch_multiple_authed.php";
 
 query("
 SELECT DISTINCT firstname AS 'firstName', lastname AS 'lastName', birthday, email
