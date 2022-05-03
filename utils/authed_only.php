a1b642100802f39cf5c06a157e6b5a2977db437e
Florian Reisinger <reisi007@gmail.com>
2022-05-03 18:31:14 +0200

    Add transaction to authed only

--- utils/authed_only.php
+++ utils/authed_only.php
@@ -7,12 +7,16 @@ $user = trim($headers['Email']);
 $pwd = trim($headers['Accesskey']);
 
 $pdo = createMysqlConnection();
+$pdo->beginTransaction();
 
 $statement = $pdo->prepare("UPDATE permission_session SET last_used = CURRENT_TIMESTAMP WHERE user_id = :user AND hash = :hash");
 $statement->bindParam("user", $user);
 $statement->bindParam("hash", $pwd);
 $statement->execute();
+
 if ($statement->rowCount() !== 1) {
     header("HTTP/1.1 401 Unauthorized");
+    $pdo->rollBack();
     exit();
 }
+$pdo->commit();
