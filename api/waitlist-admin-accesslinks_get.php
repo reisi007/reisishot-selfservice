415370d8b53f66183362991c569912a5892b543c
Florian Reisinger <reisi007@gmail.com>
2022-06-17 20:20:43 +0200

    Fix naming error in SQL query (auto format)

--- api/waitlist-admin-accesslinks_get.php
+++ api/waitlist-admin-accesslinks_get.php
@@ -3,8 +3,8 @@ include_once "../db/fetch_multiple_authed.php";
 
 query("
 SELECT CONCAT('https://service.reisishot.pictures/waitlist/', email, '/', access_key) AS url,
-       firstname                                                                      AS firstname,
-       lastname                                                                       AS lastname,
+       firstname                                                                      AS 'firstName',
+       lastname                                                                       AS 'lastName',
        email,
        birthday
 FROM waitlist_person
