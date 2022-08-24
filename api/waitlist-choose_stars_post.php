7ebeab78f3d4af0acde65f8cb4eceb8f3f2bf631
Florian Reisinger <reisi007@gmail.com>
2022-08-24 11:05:23 +0200

    Create endpoints for submitting rating / comment

---
+++ api/waitlist-choose_stars_post.php
@@ -0,0 +1,36 @@
+<?php
+
+include_once "../header/required.php";
+include_once "../db/fetch_single_secured.php";
+include_once "../utils/files.php";
+
+$pdo = createMysqlConnection();
+$pdo->beginTransaction();
+
+$json = read_body_json();
+$headers = getallheaders();
+
+$email = strtolower(trim($headers["Email"]));
+$access_key = trim($headers["Accesskey"]);
+
+$folder = strtoupper(trim($json["folder"]));
+$image = strtoupper(trim($json["image"]));
+$rating = $json["data"];
+
+$person = select("SELECT id FROM waitlist_person WHERE email = :email AND  access_key = :access_key", $pdo);
+
+$id = $person["id"];
+
+$insert = $pdo->prepare("REPLACE INTO review_pictures_rating(person, folder, filename, rating) VALUES(:p,:fo,:fi,:s) ");
+$insert->bindParam("p", $id);
+$insert->bindParam("fo", $folder);
+$insert->bindParam("fi", $image);
+$insert->bindParam("s", $rating);
+
+$insert->execute();
+
+if ($insert->rowCount() != 1) {
+    throw new Exception('Could not insert/replace rating');
+}
+
+$pdo->commit();
