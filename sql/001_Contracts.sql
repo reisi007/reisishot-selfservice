8e9bed4e18fcf53bcb06a5fa70ee3a9b9ccb855f
Florian Reisinger <reisi007@gmail.com>
2022-10-02 19:06:06 +0200

    Allow a dsgvo text....

--- sql/001_Contracts.sql
+++ sql/001_Contracts.sql
@@ -12,16 +12,23 @@ CREATE TABLE contract_data
     UNIQUE (hash_algo, hash_value)
 );
 
+CREATE TABLE dsgvo_data
+(
+    id       BIGINT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    markdown TEXT   NOT NULL -- Markdown of the contract
+);
 
 CREATE TABLE contract_instances
 (
     id              BIGINT        NOT NULL PRIMARY KEY AUTO_INCREMENT,
     contract_id     BIGINT        NOT NULL,
+    dsgvo_id        BIGINT        NOT NULL,
     additional_text VARCHAR(1024) NOT NULL,
     hash_algo       VARCHAR(64)   NOT NULL, -- Used algorithm for hashing
     hash_value      VARCHAR(128)  NOT NULL, -- Hash value for field markdown as calculated by hash_algo
     due_date        DATETIME      NOT NULL, -- Datum + Uhrzeit, bis zu der unterschrieben werden kann
-    FOREIGN KEY (contract_id) REFERENCES contract_data (id) ON DELETE CASCADE
+    FOREIGN KEY (contract_id) REFERENCES contract_data (id) ON DELETE CASCADE,
+    FOREIGN KEY (dsgvo_id) REFERENCES dsgvo_data (id) ON DELETE CASCADE
 );
 
 -- Stores the access "secret"
--- utils/string.php
+++ utils/string.php
@@ -1,11 +1,17 @@
 <?php
 
-function combineMd(string $base, string $additionalText)
+function combineMd(string $base, string $additionalText, string $dsgvo = "")
 {
     $placeholder = '%platzhalter%';
     if (str_contains($base, $placeholder)) {
-        return str_replace($placeholder, "\r\n{$additionalText}", $base);
+        $fullText = str_replace($placeholder, "\r\n{$additionalText}", $base);
     } else {
-        return $base . "\r\n" . $additionalText;
+        $fullText = "{$base}\r\n{$additionalText}";
     }
+
+    if (!empty(trim($dsgvo))) {
+        $fullText .= "\r\n{$dsgvo}";
+    }
+
+    return $fullText;
 }
